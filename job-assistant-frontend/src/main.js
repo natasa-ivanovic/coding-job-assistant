497e434e3ef628a1b7d23cf010295b5c4ff17934
Mario Kujundzic <mario.kujundzic.su@gmail.com>
2021-06-17 13:06:57 +0200

    Add login and axios stuff

--- job-assistant-frontend/src/main.js
+++ job-assistant-frontend/src/main.js
@@ -3,8 +3,45 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import VueAxios  from 'vue-axios'
+import Axios from 'axios'
 
 Vue.config.productionTip = false
+
+
+const role = Vue.observable({ role: localStorage.getItem('role') })
+
+Object.defineProperty(Vue.prototype, '$role', {
+  get () {
+    return role.role;
+  },
+  set (value) {
+    role.role = value;
+  }
+})
+
+// axios config
+Vue.use(VueAxios, Axios);
+
+// interceptor ako zatreba
+// Vue.axios.interceptors.response.use(undefined, function (error) {
+//   if (error) {
+//     const originalRequest = error.config;
+//     if (error.response.status === 401 && !originalRequest._retry) {
+//         originalRequest._retry = true;
+//         localStorage.clear();
+//         return router.push('/login');
+//     }
+//     return Promise.reject(error);
+//   }
+// })
+
+if (localStorage.getItem('authKey') != null) {
+  Vue.axios.defaults.headers['Authorization'] = localStorage.getItem('authKey');
+}
+
+Vue.axios.defaults.baseURL = 'http://localhost:9006/';
+
 
 new Vue({
   router,
--- job-assistant-frontend/src/router/index.js
+++ job-assistant-frontend/src/router/index.js
@@ -1,10 +1,29 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import LoginView from '../views/auth/LoginView.vue'
+import HomeAdminView from '../views/admin/HomeAdminView.vue'
+import HomeJobSeekerView from '../views/jobseeker/HomeJobSeekerView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    component: LoginView,
+    name: "LoginView",
+    path: "/login",
+  },
+  // popraviti ovo da bude ista ruta, samo da zavisi od role
+  {
+    component: HomeAdminView,
+    name: "HomeAdminView",
+    path: "/home-admin",
+  },
+  {
+    component: HomeJobSeekerView,
+    name: "HomeJobSeekerView",
+    path: "/home-jobseeker",
+  },
   {
     path: '/',
     name: 'Home',
